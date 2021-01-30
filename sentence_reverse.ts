f423ddc6a54f7de4ff640eeb3044c97b9a855932
majiddarvishnejad93 <majiddarvishnejad93@gmail.com>
2021-01-30 16:11:25 +0330

    Make the codes more readable

--- sentence_reverse.ts
+++ sentence_reverse.ts
@@ -1,9 +1,7 @@
 const reverseWords = (str: string): string => {
   const splitArr: string[] = str.split(' ');
-  const k: string[] = splitArr.map((i) => {
-    const f = i.split('');
-    const l = f.reverse();
-    return l.join('');
+  const words: string[] = splitArr.map((i) => {
+    return i.split('').reverse().join('');
   });
-  return k.join(' ');
+  return words.join(' ');
 };
